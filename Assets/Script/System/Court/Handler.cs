2a31afb7a75840e86c091d0c3d813414343ffb71
OldMoM <1090347251@qq.com>
2019-03-26 22:49:46 +0800

    枪膛元素组合库

---
+++ Assets/Script/System/Court/Handler.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Handler 
+{
+    protected Handler m_NextHandler;
+
+    public Handler(Handler theNextHandler)
+    {
+        m_NextHandler = theNextHandler;
+    }
+
+    public virtual void HandleRequest(string type)
+    {
+        if (m_NextHandler != null)
+        {
+            m_NextHandler.HandleRequest(type);
+        }
+    }
+}
